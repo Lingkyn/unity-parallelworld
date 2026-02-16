f48f9195f26410a0b2a5547cb6c87d5f659bdea4
Lingkyn <h.ling1@student.gsa.ac.uk>
2026-02-16 01:02:55 +0000

    Move Services to Infrastructure

--- Assets/_Project/Scripts/Infrastructure/Services/AssetService.cs
+++ Assets/_Project/Scripts/Infrastructure/Services/AssetService.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Persistence : MonoBehaviour
+public class AssetService : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
